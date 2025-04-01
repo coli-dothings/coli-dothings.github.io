7d984c3af2cacb803fbb42546452192ea68f1cbd
coli-dothings <maianhvo2005@gmail.com>
2025-04-02 00:38:52 +1100

    index html main establish

--- week5/cart/script.js
+++ week5/cart/script.js
@@ -1,13 +1,13 @@
 //array
 let shoppingCart = [
   { name: "T-shirt", price: 20 },
-  { name: "Jeans", price: 50},
-  { name: "Sneakers", price: 80 }
+  { name: "Jeans", price: 50 },
+  { name: "Sneakers", price: 80 },
   { name: "Backpack", price: 30 },
 ];
 console.log(shoppingCart);
 let total = 0;
 for (let i = 0; i < shoppingCart.length; i++) {
-    total = total + shoppingCart[i].price;
-    console.log(total);
-};
+  total = total + shoppingCart[i].price;
+  console.log(total);
+}
