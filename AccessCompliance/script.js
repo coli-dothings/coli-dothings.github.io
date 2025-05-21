f8d52ee3a4ae750807b145eba45342d29635eb54
coli-dothings <maianhvo2005@gmail.com>
2025-05-21 18:50:03 +1000

    finish CSS JS

---
+++ AccessCompliance/script.js
@@ -0,0 +1,21 @@
+const NavButton = document.querySelector("#nav-btn");
+const sideDrawer = document.querySelector("nav");
+console.log(sideDrawer);
+let isOpen = false;
+NavButton.addEventListener("click", openDrawer);
+
+function openDrawer() {
+  console.log("i am clicked");
+  if (!isOpen) {
+    sideDrawer.style.translate = "-200px";
+    isOpen = true;
+    console.log(isOpen);
+  } else {
+    sideDrawer.style.translate = "0px";
+    isOpen = false;
+    console.log(isOpen);
+  }
+}
+
+const my3DVideo = document.querySelector("#model-video");
+my3DVideo.playbackRate = 0.5;
