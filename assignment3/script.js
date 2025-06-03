a030121466cd9020191c3b4b8407984dffdd07e6
coli-dothings <maianhvo2005@gmail.com>
2025-06-03 21:04:47 +1000

    Updated all icons class and setup grid

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,17 +1,17 @@
-const video1 = document.querySelector("#vid1");
+// const video1 = document.querySelector("#vid1");
 
-video1.addEventListener("click", playIcon);
+// video1.addEventListener("click", playIcon);
 
-function playIcon() {
-  /* figure out a way to store every sources */
-  video1.src = "Day4.mp4";
-  video1.loop = false;
-  console.log("clicked");
-}
-video1.addEventListener("ended", revertPlay);
-function revertPlay() {
-  video1.src = "Day2.mp4";
-  video1.loop = true;
-}
+// function playIcon() {
+//   /* figure out a way to store every sources */
+//   video1.src = "Day4.mp4";
+//   video1.loop = false;
+//   console.log("clicked");
+// }
+// video1.addEventListener("ended", revertPlay);
+// function revertPlay() {
+//   video1.src = "Day2.mp4";
+//   video1.loop = true;
+// }
 
 /*This means all animation must stored in sets of 2. For ease of recall */
