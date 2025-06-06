f34f95d971fb44fa76c43917fa22e183b2cd79c5
coli-dothings <maianhvo2005@gmail.com>
2025-06-06 17:37:13 +1000

    ALL art and sound assets

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,17 +1,6 @@
-// const video1 = document.querySelector("#vid1");
+//Audio list will be called randomly onclick
+audioList = [];
 
-// video1.addEventListener("click", playIcon);
-
-// function playIcon() {
-//   /* figure out a way to store every sources */
-//   video1.src = "Day4.mp4";
-//   video1.loop = false;
-//   console.log("clicked");
-// }
-// video1.addEventListener("ended", revertPlay);
-// function revertPlay() {
-//   video1.src = "Day2.mp4";
-//   video1.loop = true;
-// }
-
-/*This means all animation must stored in sets of 2. For ease of recall */
+//Animation list is seperate for each icon.
+// Each conatin 3 variants and the respective CSS animate class
+i1x1List = [];
