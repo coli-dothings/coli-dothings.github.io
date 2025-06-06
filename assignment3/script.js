88e7838e0b5f59390dd3ed8a16fff14192f8e848
coli-dothings <maianhvo2005@gmail.com>
2025-06-06 17:59:21 +1000

    Minor update files

--- assignment3/script.js
+++ assignment3/script.js
@@ -4,3 +4,16 @@ audioList = [];
 //Animation list is seperate for each icon.
 // Each conatin 3 variants and the respective CSS animate class
 i1x1List = [];
+i1x2List = [];
+i1x2swList = [];
+i1x3List = [];
+i1x4List = [];
+i2x3List = [];
+i2x3swList = [];
+i2x4List = [];
+i3x3List = [];
+
+//BG track toggle mute
+
+//Deny sound onclick close window
+//Text appear after clicking close window too many times
