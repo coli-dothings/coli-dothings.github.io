9200c21861aa070f5326f073db28edb2411562ae
coli-dothings <maianhvo2005@gmail.com>
2025-10-25 16:14:36 +1100

    json compiling method

--- RMITshowcase2025/data.js
+++ RMITshowcase2025/data.js
@@ -1,10 +1,39 @@
 // DATA JS. Compile JSON data of each student here, and linked in both HTML pages for global scope
+// remember to add student ID back into the list, as well as links
 
-// Import all data as list
+const jsonList = [
+  `{
+  "full_name": "Mai Anh Vo",
+  "profile_pic": "profile/profile.webp",
+  "primary_specialization": "Illustrations",
+  "secondary_skills": ["Sound Design", "Web Design"],
+  "bio": "mock bio 1",
+  "graduation_year": "2025",
+  "website": "https://www.instagram.com/coli_artsnstuffs/",
 
-/*
-const s4021256 = `{
-  "full_name": "stundet 2",
+  "key_projects": [
+    {
+      "title": "Crow and Raven exploration",
+      "project_type": "graphics",
+      "project_cover_image": "projects/project1.webp",
+      "youtube_link": ""
+    },
+    {
+      "title": "KAMIGOROSHI re-sound deisgn",
+      "project_type": "sound",
+      "project_cover_image": "projects/project2.webp",
+      "youtube_link": "https://youtu.be/tjJ8NZKgpX4"
+    },
+    {
+      "title": "WARA WAR@",
+      "project_type": "UXUI",
+      "project_cover_image": "projects/project3.webp",
+      "youtube_link": "https://youtu.be/avoY90IKX7I?si=U_BqG70pw39fBcoO"
+    }
+  ]
+}`,
+  `{
+  "full_name": "student 2",
   "profile_pic": "profile/profile.webp",
   "primary_specialization": "specialization 1",
   "secondary_skills": ["skill 1", "skill 2"],
@@ -32,9 +61,13 @@ const s4021256 = `{
       "youtube_link": "https://youtu.be/NCKmnppmkfg?si=IB5eTHLsKKz2hN1Z"
     }
   ]
-}`;
-const myData = JSON.parse(s4021256);
+}`,
+];
 
-console.log(myData);
-console.log(myData.key_projects[0].title);
-*/
+var studentList = [];
+for (let i = 0; i < jsonList.length; i++) {
+  studentList[i] = JSON.parse(jsonList[i]);
+}
+
+console.log("json list length is " + jsonList.length);
+console.log(studentList);
---
+++ RMITshowcase2025/mainscript.js
@@ -0,0 +1,2 @@
+console.log("main script log: " + studentList[0].key_projects[0].title);
+// testing for global value between files
