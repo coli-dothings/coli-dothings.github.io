56dcff32dd9c976b1113353811c55b453b29d604
coli-dothings <maianhvo2005@gmail.com>
2025-10-30 18:09:48 +1100

    display profile main js

--- RMITshowcase2025/data.js
+++ RMITshowcase2025/data.js
@@ -3,11 +3,12 @@
 
 const jsonList = [
   `{
+  "ID":"s4021256",
   "full_name": "Mai Anh Vo",
   "profile_pic": "profile/profile.webp",
   "primary_specialization": "Illustrations",
   "secondary_skills": ["Sound Design", "Web Design"],
-  "bio": "mock bio 1",
+  "bio": "mock bio 1 by Mai Anh Vo",
   "graduation_year": "2025",
   "website": "https://www.instagram.com/coli_artsnstuffs/",
 
@@ -15,50 +16,51 @@ const jsonList = [
     {
       "title": "Crow and Raven exploration",
       "project_type": "graphics",
-      "project_cover_image": "projects/project1.webp",
+      "project_cover_image": "s4021256/projects/project1.webp",
       "youtube_link": ""
     },
     {
       "title": "KAMIGOROSHI re-sound deisgn",
       "project_type": "sound",
-      "project_cover_image": "projects/project2.webp",
-      "youtube_link": "https://youtu.be/tjJ8NZKgpX4"
+      "project_cover_image": "s4021256/projects/project2.webp",
+      "youtube_link": "https://www.youtube.com/embed/tjJ8NZKgpX4"
     },
     {
       "title": "WARA WAR@",
       "project_type": "UXUI",
-      "project_cover_image": "projects/project3.webp",
-      "youtube_link": "https://youtu.be/avoY90IKX7I?si=U_BqG70pw39fBcoO"
+      "project_cover_image": "s4021256/projects/project3.webp",
+      "youtube_link": "https://www.youtube.com/embed/avoY90IKX7I?si=U_BqG70pw39fBcoO"
     }
   ]
 }`,
   `{
+  "ID":"s1234567",
   "full_name": "student 2",
-  "profile_pic": "profile/profile.webp",
+  "profile_pic": "profile/profile.jpg",
   "primary_specialization": "specialization 1",
   "secondary_skills": ["skill 1", "skill 2"],
-  "bio": "mock bio 2",
+  "bio": "mock bio 2 by student 2",
   "graduation_year": "2026",
-  "website": "https://www.instagram.com/coli_artsnstuffs/",
+  "website": "https://www.w3schools.com/howto/howto_js_add_class.asp",
 
   "key_projects": [
     {
       "title": "Long title of project 1",
       "project_type": "3D",
-      "project_cover_image": "projects/project1.webp",
+      "project_cover_image": "s1234567/projects/project1.jpg",
       "youtube_link": ""
     },
     {
       "title": "Long title of project 2",
       "project_type": "video",
-      "project_cover_image": "projects/project2.webp",
-      "youtube_link": "https://youtu.be/amTsBProNo4?si=X8NgW_cXcQG99n2g"
+      "project_cover_image": "s1234567/projects/project2.jpg",
+      "youtube_link": "https://www.youtube.com/embed/amTsBProNo4?si=X8NgW_cXcQG99n2g"
     },
     {
       "title": "Long title of jazz piano 3",
       "project_type": "sound",
-      "project_cover_image": "projects/project3.webp",
-      "youtube_link": "https://youtu.be/NCKmnppmkfg?si=IB5eTHLsKKz2hN1Z"
+      "project_cover_image": "s1234567/projects/project3.jpg",
+      "youtube_link": "https://www.youtube.com/embed/NCKmnppmkfg?si=IB5eTHLsKKz2hN1Z"
     }
   ]
 }`,
@@ -70,3 +72,23 @@ for (let i = 0; i < jsonList.length; i++) {
 }
 
 console.log("json list length is " + jsonList.length);
+
+//======================================================================
+// Profile update page
+
+// Test html page transfer
+/*
+const itemsArray = document.querySelectorAll(".item");
+
+// Add click event to transfer to page
+for (let i = 0; i < itemsArray.length; i++) {
+  itemsArray[i].addEventListener("click", function displayProfile() {
+    chooseProfile(i);
+  });
+}
+
+function chooseProfile(i) {
+  sessionStorage.setItem("profileNum", i);
+  window.location.href = "profile.html";
+}
+*/
--- RMITshowcase2025/mainscript.js
+++ RMITshowcase2025/mainscript.js
@@ -1,32 +1,101 @@
-//console.log("main script log: " + studentList[0].key_projects[0].title);
-// testing for global value between files
+// Creating content for item
+let contentThreeD = "";
+let contentUXUI = "";
+let contentSound = "";
+let contentVideo = "";
+let contentGraphics = "";
+
+// General content test
+let formattedContentAll = "";
 
 // indexing through each student profile
 for (let i = 0; i < studentList.length; i++) {
   for (let ii = 0; ii < studentList[i].key_projects.length; ii++) {
+    // Format content
+    /*formattedContentAll += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].full_name}</p></div>
+    </div>
+    `;*/
+
     // create a switch case for each project to organize
     // "project_type": "sound/3D/UXUI/video/graphics",
     switch (studentList[i].key_projects[ii].project_type) {
       //Use [i] to get student's info
       //Use [ii] to get project's info
-      case "sound":
-        console.log(studentList[i].key_projects[ii].title + " is sound type");
-        break;
-
-      case "graphics":
-        console.log(studentList[i].key_projects[ii].title + " is graphic type");
-        break;
-
       case "3D":
         console.log(studentList[i].key_projects[ii].title + " is 3D type");
+        contentThreeD += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].key_projects[ii].title}</p></div>
+    </div>
+    `;
         break;
 
       case "UXUI":
         console.log(studentList[i].key_projects[ii].title + " is uxui type");
+        contentUXUI += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].key_projects[ii].title}</p></div>
+    </div>
+    `;
+        break;
+
+      case "sound":
+        console.log(studentList[i].key_projects[ii].title + " is sound type");
+        contentSound += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].key_projects[ii].title}</p></div>
+    </div>
+    `;
         break;
 
       case "video":
         console.log(studentList[i].key_projects[ii].title + " is uxui type");
+        contentVideo += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].key_projects[ii].title}</p></div>
+    </div>
+    `;
+        break;
+
+      case "graphics":
+        console.log(studentList[i].key_projects[ii].title + " is graphic type");
+        contentGraphics += `
+    <div class="item" id="item${i}">
+     <img
+        class="project-img"
+        src="${studentList[i].key_projects[ii].project_cover_image}"
+        alt="project id ${i}"
+      />
+      <div class="name-plate"><p>${studentList[i].key_projects[ii].title}</p></div>
+    </div>
+    `;
         break;
 
       default:
@@ -35,18 +104,21 @@ for (let i = 0; i < studentList.length; i++) {
     }
   }
 }
+/*const testDisplay = document.querySelector("#scroll-test");
+testDisplay.innerHTML += formattedContentAll;*/
 
-// Test html page transfer
-const itemsArray = document.querySelectorAll(".item");
+const threeDDisplay = document.querySelector("#three-d-scroll");
+threeDDisplay.innerHTML += contentThreeD;
 
-// Add click event to transfer to page
-for (let i = 0; i < itemsArray.length; i++) {
-  itemsArray[i].addEventListener("click", function displayProfile() {
-    chooseProfile(i);
-  });
-}
+const uxuiDisplay = document.querySelector("#uxui-scroll");
+uxuiDisplay.innerHTML += contentUXUI;
 
-function chooseProfile(i) {
-  sessionStorage.setItem("profileNum", i);
-  window.location.href = "profile.html";
-}
+const soundDisplay = document.querySelector("#sound-scroll");
+soundDisplay.innerHTML += contentSound;
+
+const videoDisplay = document.querySelector("#video-scroll");
+videoDisplay.innerHTML += contentVideo;
+
+const graphicsDisplay = document.querySelector("#graphics-scroll");
+graphicsDisplay.innerHTML += contentGraphics;
+// Move the switch case content here
